9b570e832c1c881fc6ad5e0a74990ff3111c98f3
Karol <kovalczyk97@gmail.com>
2020-09-29 14:55:54 +0200

    Basic version of auth system

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 
 
-router.get('/test', (req, res) => {
-      res.send("Hello, its working");
-})
+
 
 
 router.post('/register', async (req, res) => {
       try {
             let { email, password, passwordCheck, displayName } = req.body;
-
+            console.log(req.body);
 
             //Validate 
             if (!email || !password || !passwordCheck)
-                  return res.status(400).json({ msg: 'Not all fields have been entered' });
+                  return res.status(400).json({ msg: 'Not all fields have been entered.' });
             if (password.length < 5)
                   return res.status(400).json({ msg: 'Password length must be at least 5 characters.' });
             if (password !== passwordCheck)
@@ -45,8 +45,76 @@ router.post('/register', async (req, res) => {
             res.json(savedUser);
 
       } catch (err) {
+            res.status(500).json({ error: err.message });
+      }
+});
+
+router.post('/login', async (req, res) => {
+      try {
+            const { email, password } = req.body;
+
+            //Validate
+            if (!email || !password)
+                  return res.status(400).json({ msg: 'Not all fields have been entered.' });
+
+            const user = await User.findOne({ email: email });
+            if (!user)
+                  return res.status(400).json({ msg: 'There is no account with this email.' });
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch)
+                  return res.status(400).json({ msg: 'Invalid logging data.' });
+
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            res.json({
+                  token,
+                  user: {
+                        id: user._id,
+                        displayName: user.displayName,
+                        email: user.email,
+                  },
+            });
+
+      } catch (err) {
             res.status(500).json({ error: err.message })
       }
+})
+
+router.delete('delete', auth, async (req, res) => {
+      try {
+            const deletedUser = await User.findByIdAndDelete(req.user);
+            res.json(deletedUser);
+
+      } catch (err) {
+            res.status(500).json({ error: err.message })
+      }
+
+});
+
+
+router.post("/tokenIsValid", async (req, res) => {
+      try {
+            const token = req.header("x-auth-token");
+            if (!token) return res.json(false);
+            const verified = jwt.verify(token, process.env.JWT_SECRET)
+            if (!verified) return res.json(false);
+            const user = await User.findById(verified.id)
+            if (!user) return res.json(false);
+
+            return res.json(true);
+
+      } catch (err) {
+            res.status(500).json({ error: err.message })
+      }
+
+});
+
+router.get("/", auth, async (req, res) => {
+      const user = await User.findById(req.user);
+      res.json({
+            displayName: user.displayName,
+            id: user._id,
+      })
 })
 
 module.exports = router;
